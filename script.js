26c74dccaa489854ab88ad6a15685bb8ca8af043
Suraj Kumar Behera <113046759+Skb08@users.noreply.github.com>
2023-01-26 14:47:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 
 const options = {
 	method: 'GET',
-	// headers: {
-	// 	'X-RapidAPI-Key': 'fd99600297msh876ca6a9b17373ap145253jsn999e715ef7da',
-	// 	'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	// }
 };
 const getWeather = (id) => {
 	idName.innerHTML = id;
